3a4498cd199208a2a0ebff7be18658fc10347418
Tomasz Brzezinski <webadmin@pan.olsztyn.pl>
2026-08-03 09:33:58 +0200

    Remove redundant logo alternative text

--- template-parts/header/inlife-branding.php
+++ template-parts/header/inlife-branding.php
@@ -10,9 +10,6 @@ $logo_en = get_stylesheet_directory_uri() . '/assets/images/InLife-logo-EN.png';
 $current_lang = function_exists( 'pll_current_language' ) ? pll_current_language( 'slug' ) : 'pl';
 $logo_src = ( 'en' === $current_lang ) ? $logo_en : $logo_pl;
 
-$logo_alt = ( 'en' === $current_lang )
-	? __( 'InLife Institute logo', 'newinlife-child' )
-	: __( 'Logo Instytutu InLife', 'newinlife-child' );
 ?>
 
 <div class="site-branding">
@@ -20,7 +17,7 @@ $logo_alt = ( 'en' === $current_lang )
 		<img
 			src="<?php echo esc_url( $logo_src ); ?>"
 			class="img-fluid custom-logo"
-			alt="<?php echo esc_attr( $logo_alt ); ?>"
+			alt=""
 			decoding="async"
 		>
 		<span class="visually-hidden"><?php echo esc_html( $site_name ); ?></span>
