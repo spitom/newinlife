37644f0484c3ca3ae16dcf95b91350fd76a321a2
Tomasz Brzezinski <webadmin@pan.olsztyn.pl>
2026-08-05 09:48:54 +0200

    Use editorial layout for default pages

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Default page template.
+ *
+ * Uses the generic editable page layout.
+ *
+ * @package newinlife-child
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+require get_stylesheet_directory()
+	. '/page-templates/template-editorial-content.php';
